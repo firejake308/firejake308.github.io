71bac3201d93008ec1ecbbb8e378ae89b2adc747
Adel Hassan <firejake308@users.noreply.github.com>
2017-03-04 16:46:57 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 <head>
 <meta name="twitter:card" content="app">
 <meta name="twitter:site" content="@FuSuDiOfThWAtLa">
-<meta name="twitter:text:title" content="Show & Sell">
+<meta name="twitter:text:title" content="Show And Sell">
 <meta name="twitter:description" content="This is an app to host digital yard sales that I am designing for FBLA. It's Android-exclusive.">
-<meta name="twitter:app:name:iphone" content="Show & Sell">
+<meta name="twitter:app:name:iphone" content="Show And Sell">
 <meta name="twitter:app:id:iphone" content="">
 <meta name="twitter:app:url:iphone" content="">
-<meta name="twitter:app:name:ipad" content="Show & Sell">
+<meta name="twitter:app:name:ipad" content="Show And Sell">
 <meta name="twitter:app:id:ipad" content="">
 <meta name="twitter:app:url:ipad" content="">
-<meta name="twitter:app:name:googleplay" content="Show & Sell">
+<meta name="twitter:app:name:googleplay" content="Show And Sell">
 <meta name="twitter:app:id:googleplay" content="com.insertcoolnamehere.showandsell">
 <meta name="twitter:app:url:googleplay" content="showandsell://aad3bcc2-460c-4d98-8f11-3384fd3feff0">
 <title>Show &amp; Sell</title>
